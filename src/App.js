500c6667c3bc0f4f63c6222af8c30381c1efcee4
arjun <arjunvisibleone.arjun@gmail.com>
2024-12-10 18:54:20 +0630

    add: header layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Nav from './components/Nav';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
       <Nav/>
+
+      <Header
+        title="Little Lemon"
+        sub_title="Chicago"
+        content="We are a family owned Mediterraneran restaurnat, focused on traditional recipes servred with a mondern twist."
+        button_text = "Reserve Table"
+      />
     </>
   );
 }
